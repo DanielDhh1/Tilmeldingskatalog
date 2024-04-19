15ef23e0c76d213b22993eb8633d464e445fb01c
Nikolaj Qvist Møller <nqvistmoller@hotmail.com>
2024-04-19 10:54:03 +0200

    Yes

--- Tilmeldingskatalog/Pages/Models/Tilmeldingskatalog.cs
+++ Tilmeldingskatalog/Pages/Models/Tilmeldingskatalog.cs
@@ -2,29 +2,29 @@
 {
     public class Tilmeldingskatalog
     {       
-            List<Person> people;
+        List<Person> people;
 
 
-            public Tilmeldingskatalog()
-            {
-                people = new List<Person>();
-            }
-            public void AddPerson(Person person)
-            {
-                people.Add(person);
-
-            }
-            public void PrintPeople()
-            {
-                foreach (Person person in people)
-                {
-                    Console.WriteLine($"{person}");
-                    Console.WriteLine();
-                }
-            }
+        public Tilmeldingskatalog()
+        {
+            people = new List<Person>();
+        }
+        public void AddPerson(Person person)
+        {
+            people.Add(person);
 
         }
+        public void PrintPeople()
+        {
+            foreach (Person person in people)
+            {
+                Console.WriteLine($"{person}");
+            }
+        Console.WriteLine();
+        }
+
     }
+}
 
 
 

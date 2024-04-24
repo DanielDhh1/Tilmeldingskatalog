7ad4c16f76b8412af68537451920df42bdd30f9c
Nikolaj Qvist Møller <nqvistmoller@hotmail.com>
2024-04-24 13:37:48 +0200

    asd

--- Tilmeldingskatalog/Pages/Models/FordelingsKatalog.cs
+++ Tilmeldingskatalog/Pages/Models/FordelingsKatalog.cs
@@ -1,11 +1,34 @@
-﻿namespace Tilmeldingskatalog.Pages.Models
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Tilmeldingskatalog.Pages.Models
 {
     public class FordelingsKatalog
     {
+        public Kreds _kreds { get; set; }
         List<Kreds> _kredse;
         public FordelingsKatalog()
         {
             _kredse = new List<Kreds>();
+            _kredse.Add(new Kreds("Ugabuga", "Ugabugalugostræde", 22, 1));
+        }
+
+        public List<Kreds> NewKreds()
+        {
+            _kreds = new Kreds("","",0,0);
+            return _kredse;
+        }
+        public List<Kreds> AddKreds(Kreds kreds)
+        {
+            _kredse.Add(kreds);
+            return _kredse;
+        }
+        public void DeleteKreds(Kreds kreds)
+        {
+            _kredse.Remove(kreds);
+        }
+        public List<Kreds> GetKreds()
+        {
+            return _kredse;
         }
     }
 }
